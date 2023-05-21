c7b0e0464c2142e6282abbf717ee03d1773bcde2
liu22054037084 <liu2254037084@gmail.com>
2023-05-21 23:06:39 +0800

    2023年05月21日23时06分23秒

--- 上传备份.bat
+++ 上传备份.bat
@@ -4,7 +4,7 @@ CHCP 65001
 
 for /f "tokens=2 delims==" %%a in ('wmic path win32_operatingsystem get LocalDateTime /value') do (set t=%%a)
 
-set Today="%t:~0,4%年%t:~4,2%月%t:~6,2%日%t:~8,2%时%t:~10,2%分"
+set Today="%t:~0,4%年%t:~4,2%月%t:~6,2%日%t:~8,2%时%t:~10,2%分%t:~12,2%秒"
 
 git add .
 
