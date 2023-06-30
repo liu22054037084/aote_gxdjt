06688ba04676498464df2a97960aa240551feed7
爱则心痛 <liu2254037084@gmail.com>
2023-06-30 10:13:24 +0800

    2023年06月30日10时13分23秒

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import aote
 
 if __name__ == "__main__":
     while True:
-        try:
-            q = aote.main()
-        finally:
-            if q == 10:
-                continue
-            else:
-                break
+        q = aote.main()
+        if q == 10:
+            continue
+        else:
+            break
